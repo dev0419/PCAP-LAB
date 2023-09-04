b0fd8c07e46cd8a939175605689822e5ae2165ed
dev_0419 <52438900+dev0419@users.noreply.github.com>
2023-09-04 07:18:01 +0530

    Update l2q4.c

--- Practice/l2q4.c
+++ Practice/l2q4.c
@@ -1,25 +1,38 @@
-#include<stdio.h>
-#include<mpi.h>
-int main(int argc, char *argv[]){
-    int rank,size;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Status status;
-    int num;
-    if (rank == 0){
-        fprintf(stdout,"Enter the number:\n");
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size, n;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        fprintf(stdout, "Enter the number:\n");
         fflush(stdout);
-        scanf("%d",&num);
-        MPI_Send(&num,1,MPI_INT,1,0,MPI_COMM_WORLD);
-        fprintf(stdout,"Process(rank %d) send %d\n",rank,num);
+        scanf("%d", &n);
+        MPI_Send(&n, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        fprintf(stdout, "Process (rank %d) send %d\n", rank, n);
         fflush(stdout);
+        MPI_Recv(&n, 1, MPI_INT, size - 1, size - 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        fprintf(stdout, "Process (rank %d) received final number: %d\n", rank, n);
+        fflush(stdout);
+    } else {
+        MPI_Recv(&n, 1, MPI_INT, rank - 1, rank - 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        fprintf(stdout, "Process (rank %d) received %d\n", rank, n);
+        fflush(stdout);
+
+        n += 1;
+
+        if (rank == size - 1) {
+            MPI_Send(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        } else {
+            MPI_Send(&n, 1, MPI_INT, rank + 1, rank, MPI_COMM_WORLD);
+            fprintf(stdout, "Process (rank %d) send %d to Process (rank %d)\n", rank, n, rank + 1);
+            fflush(stdout);
+        }
     }
-    else{
-        MPI_Recv(&num,1,MPI_INT,0,0,MPI_COMM_WORLD,&status);
-        fprintf(stdout,"Process (rank %d) received %d\n",rank,num);
-        fflush(stdout);
-    }
+
     MPI_Finalize();
     return 0;
-}
+}`
