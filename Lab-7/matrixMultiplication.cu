8080ce516bf56696e7b9e2208ad1ca3d00cdc95c
dev_0419 <52438900+dev0419@users.noreply.github.com>
2023-09-19 15:36:00 +0530

    Update matrixMultiplication.cu

--- Lab-7/matrixMultiplication.cu
+++ Lab-7/matrixMultiplication.cu
@@ -1,10 +1,7 @@
-%%cuda --name prg3.cu
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-// Define the kernel to perform matrix multiplication row-wise
 __global__ void multiply_rowWise(int* a, int* b, int* c, int wa, int wb) {
     int ridA = threadIdx.x;
     int sum;
@@ -20,26 +17,26 @@ __global__ void multiply_rowWise(int* a, int* b, int* c, int wa, int wb) {
     }
 }
 
-// Define the kernel to perform matrix multiplication column-wise
+
 __global__ void multiply_Colwise(int* a, int* b, int* c, int ha, int wa) {
     int cidB = threadIdx.x;
-    int wb = blockDim.x; // Corrected variable declaration
+    int wb = blockDim.x;
 
     for (int ridA = 0; ridA < ha; ridA++) {
-        int sum = 0; // Initialize 'sum' to 0 for each column
+        int sum = 0; 
 
         for (int k = 0; k < wa; k++) {
-            sum += (a[ridA * wa + k] * b[k * wb + cidB]); // Perform element-wise multiplication and accumulate the result
+            sum += (a[ridA * wa + k] * b[k * wb + cidB]); 
         }
 
-        c[ridA * wb + cidB] = sum; // Store the accumulated 'sum' in the corresponding position of matrix 'c'
+        c[ridA * wb + cidB] = sum;
     }
 }
 
-// Define the kernel to perform matrix multiplication
+
 __global__ void multiplyKernel(int* a, int* b, int* c, int wa, int wb) {
-    int ridA = threadIdx.y; // Use threadIdx.y for rows
-    int cidB = threadIdx.x; // Use threadIdx.x for columns
+    int ridA = threadIdx.y; 
+    int cidB = threadIdx.x; 
 
     int sum = 0;
 
@@ -50,26 +47,19 @@ __global__ void multiplyKernel(int* a, int* b, int* c, int wa, int wb) {
     c[ridA * wb + cidB] = sum;
 }
 
-// Function to perform matrix multiplication
+
 void Multiply(int* a, int* b, int* c, int wa, int wb, int ha) {
-    int* d_A, * d_B, * d_C; // Declare device memory pointers
-
-    // Calculate the required memory size
+    int* d_A, * d_B, * d_C; 
     int size = ha * wb * sizeof(int);
-
-    // Allocate device memory
     cudaMalloc((void**)&d_A, size);
     cudaMalloc((void**)&d_B, size);
     cudaMalloc((void**)&d_C, size);
-
-    // Copy data from host to device
     cudaMemcpy(d_A, a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, b, size, cudaMemcpyHostToDevice);
 
-    // Launch the kernel to perform matrix multiplication row-wise
     multiply_rowWise<<<1, ha>>>(d_A, d_B, d_C, wa, wb);
 
-    // Copy the result from device to host
+
     cudaMemcpy(c, d_C, size, cudaMemcpyDeviceToHost);
 
     printf("Result of row-wise matrix multiplication:\n");
@@ -80,12 +70,8 @@ void Multiply(int* a, int* b, int* c, int wa, int wb, int ha) {
         printf("\n");
     }
 
-    // Launch the kernel to perform matrix multiplication column-wise
     multiply_Colwise<<<1, wb>>>(d_A, d_B, d_C, ha, wa);
-
-    // Copy the result from device to host
     cudaMemcpy(c, d_C, size, cudaMemcpyDeviceToHost);
-
     printf("\nResult of column-wise matrix multiplication:\n");
     for (int i = 0; i < ha; i++) {
         for (int j = 0; j < wb; j++) {
@@ -93,12 +79,9 @@ void Multiply(int* a, int* b, int* c, int wa, int wb, int ha) {
         }
         printf("\n");
     }
-
-    // Launch the kernel to perform matrix multiplication
     dim3 blockSize(wb, ha);
     multiplyKernel<<<1, blockSize>>>(d_A, d_B, d_C, wa, wb);
 
-    // Copy the result from device to host
     cudaMemcpy(c, d_C, size, cudaMemcpyDeviceToHost);
 
     printf("\nResult of matrix multiplication element-wise:\n");
@@ -109,7 +92,6 @@ void Multiply(int* a, int* b, int* c, int wa, int wb, int ha) {
         printf("\n");
     }
 
-    // Free device memory
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
@@ -123,14 +105,7 @@ int main() {
     scanf("%d %d", &ha, &wa);
     printf("Enter the dimensions of matrix B (hb wb):\n");
     scanf("%d %d", &hb, &wb);
-
-    // Check if matrix dimensions are compatible for multiplication
-    if (wa != hb) {
-        printf("Matrix dimensions are not compatible for multiplication.\n");
-        return 1;
-    }
-
-    // Allocate memory for matrices a, b, and c
+    
     a = (int*)malloc(ha * wa * sizeof(int));
     b = (int*)malloc(hb * wb * sizeof(int));
     c = (int*)malloc(ha * wb * sizeof(int));
@@ -149,10 +124,7 @@ int main() {
         }
     }
 
-    // Call the Multiply function to perform matrix multiplication
     Multiply(a, b, c, wa, wb, ha);
-
-    // Free host memory
     free(a);
     free(b);
     free(c);
