8f1120b6b7d63f45cd9715a16dd9db6f5e98e68a
dev_0419 <52438900+dev0419@users.noreply.github.com>
2023-09-20 23:33:53 +0530

    Update solved.cu

--- Lab-6/solved.cu
+++ Lab-6/solved.cu
@@ -1,82 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #define N 1024
 
-__global__ void CUDACount(char* A, int* cnt){
-    int idx = threadIdx.x;
-
-    if(A[idx] == 'a')
-        atomicAdd(cnt, 1);
+__global__ void charCount(char* a, unsigned int* d_count) {
+    int i = threadIdx.x;
+    if (a[i] == 'a')
+        atomicAdd(d_count, 1);
 }
 
-int main(){
-    char A[N];
-    char *d_A;
-
-    int count=0, *d_count, res;
-
-    cudaError_t err = cudaGetLastError();
-
-    printf("Enter String : ");
-    scanf("%[^\n]s", A);
-    printf("String : %s\n", A);
-
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 1 : %s\n", cudaGetErrorString(err));
-
+int main() {
+    char a[N], *d_A;
+    unsigned int count = 0, *d_count, result;
+    
+    printf("Enter string:\n");
+    scanf("%[^\n]s", a); // Changed from "%[^/n]s" to "%[^\n]s" for correct input
+    
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
     
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 2 : %s\n", cudaGetErrorString(err));
-
-    cudaMalloc((void**)&d_A, strlen(A)*sizeof(char));
-    cudaMalloc((void**)&d_count, sizeof(int));
-    cudaMalloc((void**)&res, sizeof(int));
-
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 3 : %s\n", cudaGetErrorString(err));
-
-    cudaMemcpy(d_count, &count, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&d_A, strlen(a) * sizeof(char)); // Changed "strlen(A)" to "strlen(a)"
+    cudaMalloc((void**)&d_count, sizeof(unsigned int));
+    cudaMemcpy(d_A, a, strlen(a) * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_count, &count, sizeof(unsigned int), cudaMemcpyHostToDevice);
     
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 3.5 : %s\n", cudaGetErrorString(err));
-        
-    cudaMemcpy(d_A, A, strlen(A)*sizeof(char), cudaMemcpyHostToDevice);
-
-
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 4 : %s\n", cudaGetErrorString(err));
-
-    CUDACount<<<1, strlen(A)>>>(d_A, d_count);
-
-    err = cudaGetLastError();
-    if(err != cudaSuccess)
-        printf("CUDA Error Occured 5 : %s\n", cudaGetErrorString(err));
-
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+        printf("Error 1: %s\n", cudaGetErrorString(err));
+    
+    charCount<<<1, strlen(a)>>>(d_A, d_count);
+    
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
-
-    float elapsedTime;
-    cudaEventElapsedTime(&elapsedTime, start, stop);
     
-
-    cudaMemcpy(&res, d_count, sizeof(int), cudaMemcpyDeviceToHost);
-    printf("Total Occurances of 'a' = %d\n", res);
-
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop); "
+    
+    cudaMemcpy(&result, d_count, sizeof(unsigned int), cudaMemcpyDeviceToHost); 
+    
+    printf("Total occurrences of 'a': %u\n", result);
+    printf("Time Taken: %f\n", elapsedTime);
+    
     cudaFree(d_A);
     cudaFree(d_count);
+    
     return 0;
-
 }
+
