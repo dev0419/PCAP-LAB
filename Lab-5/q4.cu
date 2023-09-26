408762ce194931ada542885873d0b7c5f9bae4af
Jayasuryan0821 <52438900+dev0419@users.noreply.github.com>
2023-09-26 17:05:35 +0530

    sineAngles.cu

--- Lab-5/q4.cu
+++ Lab-5/q4.cu
@@ -1,57 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-// CUDA kernel to calculate sine of angles
-__global__ void calculateSine(float *angles, float *sineResults, int numAngles) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (tid < numAngles) {
-        sineResults[tid] = sinf(angles[tid]);
+__global__ void sineAngle(float* angles, float* sineVal, int n) {
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    if (idx < n) {
+        float angle = angles[idx];
+        sineVal[idx] = sin(angle); // Assign the sine value to the output array
     }
 }
 
-int main() {
-    int numAngles = 10; // Number of angles
-    float h_angles[] = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5}; // Input angles
-    float h_sineResults[numAngles]; // Resultant sine values
-
-    // Device copies of variables angles and sineResults
-    float *d_angles, *d_sineResults;
-
-    int size = numAngles * sizeof(float); // Size of the arrays in bytes
-
-    // Allocate space for device copies of angles and sineResults
-    cudaMalloc((void **)&d_angles, size);
-    cudaMalloc((void **)&d_sineResults, size);
-
-    // Copy input angles from host to device
-    cudaMemcpy(d_angles, h_angles, size, cudaMemcpyHostToDevice);
-
-    int blockSize = 256;
-    int numBlocks = (numAngles + blockSize - 1) / blockSize;
-
-    // Launch the CUDA kernel to calculate sine values
-    calculateSine<<<numBlocks, blockSize>>>(d_angles, d_sineResults, numAngles);
-
-    // Copy the result back to the host
-    cudaMemcpy(h_sineResults, d_sineResults, size, cudaMemcpyDeviceToHost);
-
-    // Display the input angles and their corresponding sine values
-    printf("Input Angles (in radians):\n");
-    for (int i = 0; i < numAngles; i++) {
-        printf("%.2f ", h_angles[i]);
-    }
-    printf("\n\nSine Results:\n");
-    for (int i = 0; i < numAngles; i++) {
-        printf("%.4f ", h_sineResults[i]);
-    }
+void printArray(float* a, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%f ", a[i]);
     printf("\n");
+}
 
-    // Cleanup: Free device memory
-    cudaFree(d_angles);
-    cudaFree(d_sineResults);
+void sinAngle(float* angles, float* sineVal, int n) {
+    float* d_Angle, *d_sinVal;
+    int size = n * sizeof(float);
+    cudaMalloc((void**)&d_Angle, size);
+    cudaMalloc((void**)&d_sinVal, size);
+    cudaMemcpy(d_Angle, angles, size, cudaMemcpyHostToDevice);
+    printf("Angles:\n");
+    printArray(angles, n);
+    sineAngle<<<ceil((float)n / 256), 256>>>(d_Angle, d_sinVal, n);
+    cudaMemcpy(sineVal, d_sinVal, size, cudaMemcpyDeviceToHost);
+    printf("Sine Values are:\n");
+    printArray(sineVal, n);
+    cudaFree(d_Angle);
+    cudaFree(d_sinVal);
+}
 
+int main() {
+    float* angles, *sinVal;
+    int n;
+    printf("Enter the size of the angles array:\n");
+    scanf("%d", &n);
+    int size = n * sizeof(float);
+    angles = (float*)malloc(size);
+    sinVal = (float*)malloc(size);
+    printf("Enter the angles in the array:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%f", &angles[i]);
+    sinAngle(angles, sinVal, n);
+    free(angles);
+    free(sinVal);
     return 0;
 }
