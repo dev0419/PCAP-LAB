dcba83496c2d096d076bcfe451d6d89a90dba9ea
Jayasuryan0821 <52438900+dev0419@users.noreply.github.com>
2023-10-26 18:58:59 +0530

    Update oddEvenTranspose.cu

--- Lab-10/oddEvenTranspose.cu
+++ Lab-10/oddEvenTranspose.cu
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cuda_runtime.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<cuda_runtime.h>
 
-__global__ void odd(int *a, int n) {
-    int tid = blockDim.x*blockIdx.x + threadIdx.x ;
-    if (tid%2 != 0 && tid + 1 < n) {
-        if (a[tid] > a[tid + 1]) {
+__global__ void odd(int* a,int n){
+    int tid = blockIdx.x*blockDim.x + threadIdx.x;
+    if(tid % 2 != 0 && tid + 1 < n){
+        if(a[tid] > a[tid + 1]){
             int temp = a[tid];
             a[tid] = a[tid + 1];
             a[tid + 1] = temp;
@@ -13,10 +13,10 @@ __global__ void odd(int *a, int n) {
     }
 }
 
-__global__ void even(int *a, int n) {
-    int tid = blockDim.x*blockIdx.x + threadIdx.x;
-    if (tid % 2 == 0 && tid + 1 < n) {
-        if (a[tid] > a[tid + 1]) {
+__global__ void even(int* a,int n){
+    int tid = blockIdx.x*blockDim.x + threadIdx.x;
+    if(tid % 2 == 0 && tid + 1 < n){
+        if(a[tid] > a[tid + 1]){
             int temp = a[tid];
             a[tid] = a[tid + 1];
             a[tid + 1] = temp;
@@ -24,26 +24,27 @@ __global__ void even(int *a, int n) {
     }
 }
 
-int main() {
-    int *a, n, *d_a;
-    printf("Enter the size of the array:\n");
+int main(){
+    int* a,n,*da;
+    printf("Enter n:\n");
     scanf("%d", &n);
     printf("Enter the array elements:\n");
-    a = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    a = (int*)malloc(sizeof(int)*n);
+    for(int i = 0;i < n;i++){
+        scanf("%d",&a[i]);
     }
-    cudaMalloc((void**)&d_a, sizeof(int) * n);
-    cudaMemcpy(d_a, a, sizeof(int) * n, cudaMemcpyHostToDevice);
-    for (int i = 0; i < n / 2; i++) {
-        odd<<<1, n>>>(d_a, n);
-        even<<<1, n>>>(d_a, n);
+    cudaMalloc((void**)&da,sizeof(int)*n);
+    cudaMemcpy(da,a,sizeof(int)*n,cudaMemcpyHostToDevice);
+    for(int i = 0;i < n;i++){
+        odd<<<1,n>>>(da,n);
+        even<<<1,n>>>(da,n);
     }
-    cudaMemcpy(a, d_a, sizeof(int) * n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(a,da,sizeof(int)*n,cudaMemcpyDeviceToHost);
     printf("Result:\n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    cudaFree(d_a);
+    for(int i = 0;i < n;i++){
+        printf("%d ",a[i]);
+    }
+    cudaFree(da);
     free(a);
     return 0;
 }
