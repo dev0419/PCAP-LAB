091ec8e98db3073e5f879c2d396dd461051d1304
Jayasuryan0821 <52438900+dev0419@users.noreply.github.com>
2023-10-29 18:41:00 +0530

    Update tiledMatrixMultiplication.cu

--- Lab-10/tiledMatrixMultiplication.cu
+++ Lab-10/tiledMatrixMultiplication.cu
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cuda_runtime.h>
-#define BLOCK_WIDTH 2
 #define TILE_WIDTH 2
 #define WIDTH 4
 
 __global__ void matMul(int* a,int* b,int* c){
     __shared__ int MD[TILE_WIDTH][TILE_WIDTH];
-    __shared__  int ND[TILE_WIDTH][TILE_WIDTH];
-    int m;
+    __shared__ int ND[TILE_WIDTH][TILE_WIDTH];
     int bx = blockIdx.x;
     int by = blockIdx.y;
     int tx = threadIdx.x;
@@ -16,52 +14,51 @@ __global__ void matMul(int* a,int* b,int* c){
     int row = by*TILE_WIDTH + ty;
     int col = bx*TILE_WIDTH + tx;
     int pval = 0;
+    int m;
     for(m = 0;m < WIDTH/TILE_WIDTH;m++){
-        MD[tx][ty] = a[row*WIDTH + m * TILE_WIDTH+tx];
-        ND[tx][ty] = b[(m*TILE_WIDTH + ty)* WIDTH+col];
-        __syncthreads();
-        for(int k = 0;k < TILE_WIDTH;k++){
-            pval += MD[ty][k]*ND[k][tx];
-        }
-
+        MD[tx][ty] = a[row*WIDTH + m*TILE_WIDTH + tx];
+        ND[tx][ty] = b[m*TILE_WIDTH+ ty*WIDTH + col];
         __syncthreads();
     }
+    for(int k = 0;k < TILE_WIDTH;k++){
+        pval += MD[ty][k] * ND[k][tx];
+    }
+    __syncthreads();
     c[row*WIDTH + col] = pval;
 }
 
-
 int main(){
-    int* matA,*matB,*matC,*d_a,*d_b,*d_c;
-    matA = (int*)malloc(WIDTH*WIDTH*sizeof(int));
-    printf("Enter the elements of 4x4 matA:\n");
+    int* a,*b,*c,*da,*db,*dc;
+    a = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    b = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    c = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    printf("Enter the 4x4 matrix a:\n");
     for(int i = 0;i < WIDTH*WIDTH;i++)
-      scanf("%d",&matA[i]);
-    matB = (int*)malloc(WIDTH*WIDTH*sizeof(int));
-    printf("Enter the elements of 4x4 matB:\n");
+        scanf("%d",&a[i]);
+    printf("Enter the 4x4 matrix b:\n");
     for(int i = 0;i < WIDTH*WIDTH;i++)
-      scanf("%d",&matB[i]);
-    matC = (int*)malloc(WIDTH*WIDTH*sizeof(int));
-    cudaMalloc((void**)&d_a,sizeof(int)*WIDTH*WIDTH);
-    cudaMalloc((void**)&d_b,sizeof(int)*WIDTH*WIDTH);
-    cudaMalloc((void**)&d_c,sizeof(int)*WIDTH*WIDTH);
-    cudaMemcpy(d_a,matA,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b,matB,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
+        scanf("%d",&b[i]);
+    cudaMalloc((void**)&da,sizeof(int)*WIDTH*WIDTH);
+    cudaMalloc((void**)&db,sizeof(int)*WIDTH*WIDTH);
+    cudaMalloc((void**)&dc,sizeof(int)*WIDTH*WIDTH);
+    cudaMemcpy(da,a,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
+    cudaMemcpy(db,b,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
     dim3 grid_conf(WIDTH/TILE_WIDTH,WIDTH/TILE_WIDTH);
     dim3 block_conf(TILE_WIDTH,TILE_WIDTH);
-    matMul<<<grid_conf,block_conf>>>(d_a,d_b,d_c);
-    cudaMemcpy(matC,d_c,sizeof(int)*WIDTH*WIDTH,cudaMemcpyDeviceToHost);
+    matMul<<<grid_conf,block_conf>>>(da,db,dc);
+    cudaMemcpy(c,dc,sizeof(int)*WIDTH*WIDTH,cudaMemcpyDeviceToHost);
     printf("Result:\n");
     for(int i = 0;i < WIDTH;i++){
-      for(int j = 0;j < WIDTH;j++){ 
-        printf("%d ",matC[i*WIDTH + j]);
-      }
-      printf("\n");
+        for(int j = 0;j < WIDTH;j++){
+            printf("%d ",c[i*WIDTH + j]);
+        }
+        printf("\n");
     }
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-    free(matA);
-    free(matB);
-    free(matC);
+    cudaFree(da);
+    cudaFree(db);
+    cudaFree(dc);
+    free(a);
+    free(b);
+    free(c);
     return 0;
 }
