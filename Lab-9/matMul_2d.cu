6da4855867e96f32d242c693e63dd3ad3b7e4a89
Jayasuryan0821 <52438900+dev0419@users.noreply.github.com>
2023-10-29 16:14:19 +0530

    Update matMul_2d.cu

--- Lab-9/matMul_2d.cu
+++ Lab-9/matMul_2d.cu
@@ -1,58 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cuda_runtime.h>
-#define TILE_WIDTH 2
+#define TILE_WIDTH 2 
 #define WIDTH 4
 __global__ void matMul(int* a,int* b,int* c){
-    int row = threadIdx.y + blockDim.y*blockIdx.y;
-    int col = threadIdx.x + blockDim.x*blockIdx.x;
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    int pval = 0;
     if(row < WIDTH && col < WIDTH){
-        int sum = 0;
-        for(int k = 0;k < WIDTH;k++){
-            sum += a[row*WIDTH + k]*b[k*WIDTH + col];
-        }
-        c[row*WIDTH + col] = sum;
+        for(int k = 0;k < WIDTH;k++)
+            pval += a[row*WIDTH + k]*b[k*WIDTH + col];
+        c[row*WIDTH + col] = pval;
     }
 }
 
-int main(){
-    int* matA,*matB,*matC,*da,*db,*dc;
-    matA = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
-    matB = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
-    matC = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
-    printf("Enter the elements of matrix of A (4x4):\n");
-    for(int i = 0;i < WIDTH;i++){
-        for(int j = 0;j < WIDTH;j++){
-            scanf("%d",&matA[i*WIDTH + j]);
-        }
-    }
-    printf("Enter the elements of matrix of B (4x4):\n");
-    for(int i = 0;i < WIDTH;i++){
-        for(int j = 0;j < WIDTH;j++){
-            scanf("%d",&matB[i*WIDTH + j]);
-        }
-    }
-    cudaMalloc((void**)&da,WIDTH*WIDTH*sizeof(int));
-    cudaMalloc((void**)&db,WIDTH*WIDTH*sizeof(int));
-    cudaMalloc((void**)&dc,WIDTH*WIDTH*sizeof(int));
-    cudaMemcpy(da,matA,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
-    cudaMemcpy(db,matB,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
+void perform_matMul(int* a,int* b,int* c){
+    int* da,*db,*dc,size;
+    size = sizeof(int)*WIDTH*WIDTH;
+    cudaMalloc((void**)&da,size);
+    cudaMalloc((void**)&db,size);
+    cudaMalloc((void**)&dc,size);
+    cudaMemcpy(da,a,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(db,b,size,cudaMemcpyHostToDevice);
     dim3 grid_conf(WIDTH/TILE_WIDTH,WIDTH/TILE_WIDTH);
-    dim3 block_conf(TILE_WIDTH,TILE_WIDTH);
+    dim3 block_conf(WIDTH,WIDTH);
     matMul<<<grid_conf,block_conf>>>(da,db,dc);
-    cudaMemcpy(matC,dc,sizeof(int)*WIDTH*WIDTH,cudaMemcpyDeviceToHost);
-    printf("Result:\n");
-    for(int i = 0;i < WIDTH;i++){
-        for(int j = 0;j < WIDTH;j++){
-            printf("%d ",matC[i*WIDTH+j]);
-        }
-        printf("\n");
-    }
+    cudaMemcpy(c,dc,size,cudaMemcpyDeviceToHost);
     cudaFree(da);
     cudaFree(db);
     cudaFree(dc);
-    free(matA);
-    free(matB);
-    free(matC);
+}
+
+int main(){
+    int* a,*b,*c;
+    a = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    b = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    c = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
+    printf("Enter 4x4 matrix A:\n");
+    for(int i = 0;i < WIDTH*WIDTH;i++)
+        scanf("%d",&a[i]);
+    printf("Enter 4x4 matrix B:\n");
+    for(int i = 0;i < WIDTH*WIDTH;i++)
+        scanf("%d",&b[i]);
+    perform_matMul(a,b,c);
+    printf("Result:\n");
+    for(int i = 0;i < WIDTH;i++){
+        for(int j = 0;j < WIDTH;j++){
+            printf("%d ",c[i*WIDTH + j]);
+        }
+        printf("\n");
+    }
+    free(a);
+    free(b);
+    free(c);
     return 0;
 }
