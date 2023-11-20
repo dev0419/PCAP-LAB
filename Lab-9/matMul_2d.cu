e3f2a528673a20d668e80a0557ddceb5a4cc1c14
Jayasuryan0821 <52438900+dev0419@users.noreply.github.com>
2023-11-20 22:58:37 +0530

    Update matMul_2d.cu

--- Lab-9/matMul_2d.cu
+++ Lab-9/matMul_2d.cu
@@ -1,57 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<cuda_runtime.h>
-#define TILE_WIDTH 2 
+#define TILE_WIDTH 2
 #define WIDTH 4
+
 __global__ void matMul(int* a,int* b,int* c){
     int row = blockIdx.y*blockDim.y + threadIdx.y;
     int col = blockIdx.x*blockDim.x + threadIdx.x;
-    int pval = 0;
+    int res = 0;
     if(row < WIDTH && col < WIDTH){
-        for(int k = 0;k < WIDTH;k++)
-            pval += a[row*WIDTH + k]*b[k*WIDTH + col];
-        c[row*WIDTH + col] = pval;
+        for(int k = 0;k < WIDTH;k++){
+            res = a[row*WIDTH + k]*b[k*WIDTH + col];
+        }
+        c[row*WIDTH + col] = res;
     }
-}
-
-void perform_matMul(int* a,int* b,int* c){
-    int* da,*db,*dc,size;
-    size = sizeof(int)*WIDTH*WIDTH;
-    cudaMalloc((void**)&da,size);
-    cudaMalloc((void**)&db,size);
-    cudaMalloc((void**)&dc,size);
-    cudaMemcpy(da,a,size,cudaMemcpyHostToDevice);
-    cudaMemcpy(db,b,size,cudaMemcpyHostToDevice);
-    dim3 grid_conf(WIDTH/TILE_WIDTH,WIDTH/TILE_WIDTH);
-    dim3 block_conf(WIDTH,WIDTH);
-    matMul<<<grid_conf,block_conf>>>(da,db,dc);
-    cudaMemcpy(c,dc,size,cudaMemcpyDeviceToHost);
-    cudaFree(da);
-    cudaFree(db);
-    cudaFree(dc);
 }
 
 int main(){
-    int* a,*b,*c;
+    int* a,*b,*c,*da,*db,*dc;
     a = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
     b = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
     c = (int*)malloc(sizeof(int)*WIDTH*WIDTH);
-    printf("Enter 4x4 matrix A:\n");
+    printf("Enter 4x4 matrix a:\n");
     for(int i = 0;i < WIDTH*WIDTH;i++)
         scanf("%d",&a[i]);
-    printf("Enter 4x4 matrix B:\n");
+    printf("Enter 4x4 matrix b:\n");
     for(int i = 0;i < WIDTH*WIDTH;i++)
         scanf("%d",&b[i]);
-    perform_matMul(a,b,c);
+    cudaMalloc((void**)&da,sizeof(int)*WIDTH*WIDTH);
+    cudaMalloc((void**)&db,sizeof(int)*WIDTH*WIDTH);
+    cudaMalloc((void**)&dc,sizeof(int)*WIDTH*WIDTH);
+    cudaMemcpy(da,a,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
+    cudaMemcpy(db,b,sizeof(int)*WIDTH*WIDTH,cudaMemcpyHostToDevice);
+    dim3 grid_conf(WIDTH/TILE_WIDTH,WIDTH/TILE_WIDTH);
+    dim3 block_conf(TILE_WIDTH,TILE_WIDTH);
+    matMul<<<grid_conf,block_conf>>>(da,db,dc);
+    cudaMemcpy(c,dc,sizeof(int)*WIDTH*WIDTH,cudaMemcpyDeviceToHost);
     printf("Result:\n");
-    for(int i = 0;i < WIDTH;i++){
-        for(int j = 0;j < WIDTH;j++){
-            printf("%d ",c[i*WIDTH + j]);
+    for(int i = 0; i < WIDTH;i++){
+        for(int j = 0; j < WIDTH;j++){
+            printf("%d ",c[i*WIDTH + j]);       
         }
         printf("\n");
     }
-    free(a);
-    free(b);
-    free(c);
     return 0;
 }
